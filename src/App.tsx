eb10a327a13151e3d1165180603783caec3ec632
matiasj899 <matiasjuarez899@gmail.com>
2021-07-28 09:34:26 -0300

    add route for each page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Catalogo from './pages/Catalogo/Catalogo'
+import Producto from './pages/Producto/Producto';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/catalogo" component={Catalogo} />
+          <Route exact path="/producto/:id" component={Producto} />
+        
+        </Switch>
+      </Router>
     </div>
   );
 }
