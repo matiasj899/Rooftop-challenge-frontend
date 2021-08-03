c7b06d59882256f27ccdcc2127f28fc3981c94b4
matiasj899 <matiasjuarez899@gmail.com>
2021-08-03 15:22:16 -0300

    add details list of product

--- src/pages/Producto/Producto.tsx
+++ src/pages/Producto/Producto.tsx
@@ -1,19 +1,19 @@
 import React, { useEffect, useState } from "react";
 import clienteAxios from "../../config/axios";
 import Header from "../../components/Header";
-import ProductImages from "./ProductImages";
+
 import ImageGallery from "react-image-gallery";
-import './Producto.css'
+import "./Producto.css";
 const Producto = (props: any) => {
   const productId = props.match.params.id;
   const Product = {
     images: [],
     id: "",
     currency: "",
-    features: [],
+    features: [{ key: "", name: "", value: "" }],
     title: "",
     price: "",
-    offer: {price:null},
+    offer: { price: null },
   };
   const [products, setProducts] = useState(Product);
   useEffect(() => {
@@ -28,28 +28,24 @@ const Producto = (props: any) => {
       });
   }, [productId]);
   console.log(products);
-  {
-    /*const images: Array<any> = [
-    {
-      original: products.images,
-    },
-    {
-      original: products.images,
-    },
-    {
-      original: products.images,
-    },
-  ];*/
-  }
+
   let images: any = [];
   const newImagesArray = products.images.forEach((image) =>
     images.push({ original: image })
   );
+  const features = products.features.map((feature) => {
+    return (
+      <tr key={feature.key}>
+        <th>{feature.name}</th>
+        <td>{feature.value}</td>
+      </tr>
+    );
+  });
   return (
     <>
       <Header></Header>
-      <div id='productDetail-cn'>
-        <div className='productDetail-img-info-cn'>
+      <div id="productDetail-cn">
+        <div className="productDetail-img-info-cn">
           <div className="imageGallery-cn">
             <ImageGallery
               showPlayButton={false}
@@ -58,14 +54,31 @@ const Producto = (props: any) => {
               showThumbnails={false}
               items={images}
             ></ImageGallery>
+            <div className="productDetail-info-cn">
+              <h2>{products.title}</h2>
+              {products.offer !== null ? (
+                <div>
+                  <p className="before-price">
+                    <span>{products.currency}</span>
+                    {products.price}
+                  </p>
+                  <p className="sale-price">
+                    <span>{products.currency}</span>
+                    {products.offer.price}
+                  </p>
+                </div>
+              ) : (
+                <p>{products.price}</p>
+              )}
+              <button>Comprar ahora</button>
+            </div>
           </div>
-          <div className="productDetail-info-cn">
-          <h2>{products.title}</h2>
-          {products.offer!==null?<div>
-            <p className='before-price'>{products.price}</p>
-            <p>{products.offer.price}</p></div>:<p>{products.price}</p>}
+          <div className="features-cn">
+            <h3>Caracteristicas principales</h3>
+            <table>
+              <tbody>{features}</tbody>
+            </table>
           </div>
-          
         </div>
       </div>
     </>
