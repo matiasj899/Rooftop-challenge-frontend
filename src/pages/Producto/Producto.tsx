f0e728177b9a9cd5d34d59a9d01905cc9c309009
matiasj899 <matiasjuarez899@gmail.com>
2021-08-03 00:13:01 -0300

    add image gallery and  styles to product detail page

--- src/pages/Producto/Producto.tsx
+++ src/pages/Producto/Producto.tsx
@@ -3,7 +3,7 @@ import clienteAxios from "../../config/axios";
 import Header from "../../components/Header";
 import ProductImages from "./ProductImages";
 import ImageGallery from "react-image-gallery";
-
+import './Producto.css'
 const Producto = (props: any) => {
   const productId = props.match.params.id;
   const Product = {
@@ -13,7 +13,7 @@ const Producto = (props: any) => {
     features: [],
     title: "",
     price: "",
-    offer: {},
+    offer: {price:null},
   };
   const [products, setProducts] = useState(Product);
   useEffect(() => {
@@ -21,14 +21,15 @@ const Producto = (props: any) => {
       .get(`/items/${productId}`)
       .then((res) => {
         console.log(res);
-        setProducts({...Product,...res.data});
+        setProducts({ ...Product, ...res.data });
       })
       .catch((error) => {
         console.log(error);
       });
   }, [productId]);
   console.log(products);
-  {/*const images: Array<any> = [
+  {
+    /*const images: Array<any> = [
     {
       original: products.images,
     },
@@ -38,26 +39,35 @@ const Producto = (props: any) => {
     {
       original: products.images,
     },
-  ];*/}
-  let images:any=[]
-  const newImagesArray=products.images.forEach(image=>images.push({original:image}))
+  ];*/
+  }
+  let images: any = [];
+  const newImagesArray = products.images.forEach((image) =>
+    images.push({ original: image })
+  );
   return (
     <>
       <Header></Header>
-
-      <h1>{products.title}</h1>
-      <div>
-
-      <ImageGallery
-        
-        showPlayButton={false}
-        autoPlay={true}
-        showBullets={true}
-        showThumbnails={false}
-        items={images}
-      ></ImageGallery>
+      <div id='productDetail-cn'>
+        <div className='productDetail-img-info-cn'>
+          <div className="imageGallery-cn">
+            <ImageGallery
+              showPlayButton={false}
+              autoPlay={true}
+              showBullets={true}
+              showThumbnails={false}
+              items={images}
+            ></ImageGallery>
+          </div>
+          <div className="productDetail-info-cn">
+          <h2>{products.title}</h2>
+          {products.offer!==null?<div>
+            <p className='before-price'>{products.price}</p>
+            <p>{products.offer.price}</p></div>:<p>{products.price}</p>}
+          </div>
+          
+        </div>
       </div>
-
     </>
   );
 };
